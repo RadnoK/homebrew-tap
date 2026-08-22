012d6271447072c6c64e24927272c7d6c855087e
Konrad Alfaro <alfaro.konrad@gmail.com>
2026-08-22 09:46:57 +0200

    fix: sha256 dla zte-menu 0.1.0

--- Casks/zte-menu.rb
+++ Casks/zte-menu.rb
@@ -1,6 +1,6 @@
 cask "zte-menu" do
   version "0.1.0"
-  sha256 "PLACEHOLDER_SHA256"
+  sha256 "5c64b9330ad35c7e876e312ed97864fe1aad775540729dee0c14cdde98a95a75"
 
   url "https://github.com/RadnoK/zte-menu/releases/download/v#{version}/ZteMenu-#{version}.zip"
   name "ZTE Menu"
