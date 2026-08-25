a6868f04accdc7cdfa51fc9e202c2c9330ceaf6e
Konrad Alfaro <alfaro.konrad@gmail.com>
2026-08-25 14:01:06 +0200

    fix: align cask token and URLs with the router-menu rename

--- Casks/router-menu.rb
+++ Casks/router-menu.rb
@@ -1,11 +1,11 @@
-cask "zte-menu" do
+cask "router-menu" do
   version "0.4.1"
   sha256 "97cef11cae1ac4834b2d7403c3f29957be9405a69276b0ecfb54f040bc04b543"
 
-  url "https://github.com/RadnoK/zte-menu/releases/download/v#{version}/ZteMenu-#{version}.zip"
-  name "ZTE Menu"
-  desc "Menu bar app showing ZTE U50 5G modem status"
-  homepage "https://github.com/RadnoK/zte-menu"
+  url "https://github.com/RadnoK/router-menu/releases/download/v#{version}/ZteMenu-#{version}.zip"
+  name "Router Menu"
+  desc "Menu bar status for ZTE modems and ASUS routers"
+  homepage "https://github.com/RadnoK/router-menu"
 
   depends_on macos: :sonoma
 
@@ -16,5 +16,6 @@ cask "zte-menu" do
   zap trash: [
     "~/Library/Application Support/zte-menu",
     "~/Library/Preferences/io.8lines.zte-menu.plist",
+    "~/Library/Preferences/io.8lines.router-menu.plist",
   ]
 end
