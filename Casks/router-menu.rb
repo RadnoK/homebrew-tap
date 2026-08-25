c5c7d8b15a6c03b7157bffb8b39bd53a96c9928b
Konrad Alfaro <alfaro.konrad@gmail.com>
2026-08-25 14:33:19 +0200

    chore: Router Menu app and zip names for 0.5.1

--- Casks/router-menu.rb
+++ Casks/router-menu.rb
@@ -2,7 +2,7 @@ cask "router-menu" do
   version "0.5.0"
   sha256 "368cebf05dfa9402abbcd55d16a77f1bed10a7c9d97af4430bd0d4df6598b456"
 
-  url "https://github.com/RadnoK/router-menu/releases/download/v#{version}/ZteMenu-#{version}.zip"
+  url "https://github.com/RadnoK/router-menu/releases/download/v#{version}/RouterMenu-#{version}.zip"
   name "Router Menu"
   desc "Menu bar status for ZTE modems and ASUS routers"
   homepage "https://github.com/RadnoK/router-menu"
@@ -11,7 +11,7 @@ cask "router-menu" do
 
   auto_updates true
 
-  app "ZTE Menu.app"
+  app "Router Menu.app"
 
   zap trash: [
     "~/Library/Application Support/zte-menu",
